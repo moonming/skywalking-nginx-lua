4378f74c18c4ee5fa782aa4f8fe4bbc47781aa4f
WenMing <moonbingbing@gmail.com>
2020-02-24 21:28:13 +0800

    fix code style.

--- lib/skywalking/util.lua
+++ lib/skywalking/util.lua
@@ -1,34 +1,35 @@
--- 
+--
 -- Licensed to the Apache Software Foundation (ASF) under one or more
 -- contributor license agreements.  See the NOTICE file distributed with
 -- this work for additional information regarding copyright ownership.
 -- The ASF licenses this file to You under the Apache License, Version 2.0
 -- (the "License"); you may not use this file except in compliance with
 -- the License.  You may obtain a copy of the License at
--- 
+--
 --    http://www.apache.org/licenses/LICENSE-2.0
--- 
+--
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
--- 
+--
 
-local Util = {}
+local _M = {}
+
 local MAX_ID_PART2 = 1000000000
 local MAX_ID_PART3 = 100000
 local SEQ = 1
 
-function Util:newID()
+function _M.newID()
     SEQ = SEQ + 1
-    return {Util.timestamp(), math.random( 0, MAX_ID_PART2), math.random( 0, MAX_ID_PART3) + SEQ}
+    return {_M.timestamp(), math.random( 0, MAX_ID_PART2), math.random( 0, MAX_ID_PART3) + SEQ}
 end
 
 -- Format a trace/segment id into an array.
 -- An official ID should have three parts separated by '.' and each part of it is a number
-function Util:formatID(str) 
-    local parts = Util:split(str, '.')
+function _M.formatID(str)
+    local parts = _M.split(str, '.')
     if #parts ~= 3 then
         return nil
     end
@@ -41,26 +42,26 @@ function Util:formatID(str)
 end
 
 -- @param id is an array with length = 3
-function Util:id2String(id)
+function _M.id2String(id)
     return id[1] .. '.' .. id[2] .. '.' .. id[3]
 end
 
 -- A simulation implementation of Java's System.currentTimeMillis() by following the SkyWalking protocol.
 -- Return the difference as string, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC.
 -- But in using os.clock(), I am not sure whether it is accurate enough.
-function Util:timestamp()
-    local a,b = math.modf(os.clock())
-    if b==0 then 
-        b='000' 
-    else 
-        b=tostring(b):sub(3,5) 
+function _M.timestamp()
+    local _, b = math.modf(os.clock())
+    if b==0 then
+        b='000'
+    else
+        b=tostring(b):sub(3,5)
     end
 
     return os.time() * 1000 + b
 end
 
 -- Split the given string by the delimiter. The delimiter should be a literal string, such as '.', '-'
-function Util:split(str, delimiter)
+function _M.split(str, delimiter)
     local t = {}
 
     for substr in string.gmatch(str, "[^".. delimiter.. "]*") do
@@ -73,4 +74,4 @@ function Util:split(str, delimiter)
 end
 
 
-return Util
+return _M
