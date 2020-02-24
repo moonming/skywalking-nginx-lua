1644c7a0757c6c5b4956413013c0323dba4ec24b
WenMing <moonbingbing@gmail.com>
2020-02-24 22:08:04 +0800

    fix

--- lib/skywalking/segment.lua
+++ lib/skywalking/segment.lua
@@ -1,19 +1,19 @@
--- 
+--
 -- Licensed to the Apache Software Foundation (ASF) under one or more
 -- contributor license agreements.  See the NOTICE file distributed with
 -- this work for additional information regarding copyright ownership.
 -- The ASF licenses this file to You under the Apache License, Version 2.0
 -- (the "License"); you may not use this file except in compliance with
 -- the License.  You may obtain a copy of the License at
--- 
+--
 --    http://www.apache.org/licenses/LICENSE-2.0
--- 
+--
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
--- 
+--
 
 -- Segment represents a finished tracing context
 -- Including all information to send to the SkyWalking OAP server.
@@ -61,14 +61,14 @@ end
 function Segment:transform()
     local segmentBuilder = SegmentProtocol:new()
     segmentBuilder.serviceId = self.service_id
-    segmentBuilder.globalTraceIds[1] = Util:id2String(self.trace_id)
-    segmentBuilder.traceSegmentId = Util:id2String(self.segment_id)
+    segmentBuilder.globalTraceIds[1] = Util.id2String(self.trace_id)
+    segmentBuilder.traceSegmentId = Util.id2String(self.segment_id)
     segmentBuilder.serviceId = self.service_id
     segmentBuilder.serviceInstanceId = self.service_inst_id
 
     segmentBuilder.spans = {}
     for i, span in ipairs(self.spans)
-    do 
+    do
         segmentBuilder.spans[#segmentBuilder.spans + 1] = span:transform()
     end
 
